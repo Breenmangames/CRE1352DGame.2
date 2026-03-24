7f09d0e9f38822fd71a660adb988500d4cf396a2
Finn Donnelly <Donnelly-F16@ulster.ac.uk>
2026-03-24 14:44:41 +0000

    Fixed quest scripts

--- Assets/Finn/Scripts/Quest.cs
+++ Assets/Finn/Scripts/Quest.cs
@@ -1,7 +1,7 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [CreateAssetMenu(menuName = "Quests/Quest")]
 public class Quest : ScriptableObject
