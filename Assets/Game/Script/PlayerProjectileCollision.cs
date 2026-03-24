4339e2d7c244cd394f485331a7ad5aeea3f86d99
Eoghan Breen <Breen-E18@ulster.ac.uk>
2026-03-24 13:36:59 +0000

    fixed projectile attck

---
+++ Assets/Game/Script/PlayerProjectileCollision.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class PlayerProjectileCollision : MonoBehaviour
+{
+
+    EnemyHealth EnemyHealth;
+    private int _damage = 25;
+    private Vector2 direction;
+    private float speed;
+    
+    private float knockbackForce;
+    private float lifetime = 5f;
+
+    public void Init(Vector2 fireDirection, float projectileSpeed, int projectileDamage, float knockback)
+    {
+        direction = fireDirection.normalized;
+        speed = projectileSpeed;
+        _damage = projectileDamage;
+        knockbackForce = knockback;
+
+        Destroy(gameObject, lifetime);
+    }
+
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        EnemyHealth enemyHP = other.GetComponentInParent<EnemyHealth>();
+
+        if (enemyHP != null)
+        {
+            enemyHP.TakeDamage(_damage);
+
+            Rigidbody2D enemyRb = other.GetComponentInParent<Rigidbody2D>();
+            if (enemyRb != null)
+            {
+                Vector2 knockbackDir = (other.transform.position - transform.position).normalized;
+                enemyRb.AddForce(knockbackDir * knockbackForce, ForceMode2D.Impulse);
+            }
+
+            Destroy(gameObject);
+        }
+        else if (!other.isTrigger)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
