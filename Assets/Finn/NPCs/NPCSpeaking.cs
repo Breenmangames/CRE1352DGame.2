74ebed7602a0cb503013f354d36e4dedd7d1fc10
Ickle1790 <Donnelly-F16@ulster.ac.uk>
2026-04-03 21:22:01 +0100

    struggling to add interactable system

---
+++ Assets/Finn/NPCs/NPCSpeaking.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPCSpeaking : MonoBehaviour
+{
+    public NPCDialogue dialogueData;
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText, nameText;
+    public Image portraitImage;
+
+    private int dialogueIndex;
+    private bool isTyping, isDialogueActive;
+}
