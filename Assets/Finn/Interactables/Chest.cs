128bba240c3a643eb7e53d09ad6568fbdf9ed359
Ickle1790 <Donnelly-F16@ulster.ac.uk>
2026-04-03 22:15:23 +0100

    worked on interactable scripts

---
+++ Assets/Finn/Interactables/Chest.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour, IInteractable2
+{
+    public bool IsOpened { get; private set; }
+    public string ChestID { get; private set; }
+    public GameObject itemPrefab; //The item inside the chest
+    public Sprite openedSprite;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ChestID ??= GlobalHelper.GenerateUniqueID(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool CanInteract()
+    {
+        return !IsOpened;
+    }
+
+    public void Interact()
+    {
+        if (!CanInteract()) return;
+        OpenChest();
+    }
+
+    private void OpenChest()
+    {
+        SetOpened(true);
+
+        //DropItem
+        if(itemPrefab)
+        {
+            GameObject droppedItem = Instantiate(itemPrefab, transform.position + Vector3.down, Quaternion.identity);
+        }
+    }
+
+    public void SetOpened(bool opened)
+    {
+        if (IsOpened = opened)
+        {
+            GetComponent<SpriteRenderer>().sprite = openedSprite;
+        }
+    }
+}
---
+++ Assets/Finn/Interactables/IInteractable2.cs
@@ -0,0 +1,5 @@
+public interface IInteractable2
+{
+    void Interact();
+    bool CanInteract();
+}
